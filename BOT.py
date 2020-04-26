8bc6ea344cfe1e0772f30f87059f466037effa03
Estol75 <64350850+Estol75@users.noreply.github.com>
2020-04-26 12:58:45 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,26 +1,10 @@
 import discord
 from discord.ext import commands
-import os
 
-Bot = commands.Bot(command_prefix= "!")
-Bot.remove_command('help')
 
-@Bot.event
-async def on_ready():
-    print("Bot is ready")
+bot = commands.Bot(command_prefix='!')
 
-@Bot.command(pass_context = True)
-async def корды():
-    await Bot.say('**Вот тебе полезные координаты:**\r\n'
-                      '```Ферма скелетов (По метро): -7844 -5753```'
-                      '```Ферма гвардов (по метро): -8000 -6435```'
-                      '```Ферма золота и яма: -7777 -6460```'
-                      '```Ферма зомби и пауков (По метро): -7430 -5895```'
-                      '```Городская ферма ифритов (В аду): -1200 -630```'
-                      '```Портал в энд (В аду): -1100 -950```'
-                      '```Портал в Техноград (В аду): -918 -750```'
-                      '```Портал в хаб Технограда: -7400 -6085```'
-                      '```Склад: -7470 -6015```')
+@bot.command(pass_context=True) #разрешаем передавать агрументы
+async def test(ctx, arg): #создаем асинхронную фунцию бота
+    await ctx.send(arg) #отправляем обратно аргумент
 
-token = os.environ.get('BOT_TOKEN')
-Bot.run(str(token))
