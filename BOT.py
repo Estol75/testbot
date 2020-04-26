89a19c0c2fdd7c4d52508191e30b548089a6a0e0
Estol75 <64350850+Estol75@users.noreply.github.com>
2020-04-26 14:21:58 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,15 +1,1 @@
-import discord
-from discord.ext import commands
-import os
 
-Bot = commands.Bot(command_prefix='!')
-Bot.remove_command('help')
-
-@Bot.command(pass_context = True)
-async def ping():
-    await Bot.say("pong")
-
-  
-token = os.environ.get('BOT_TOKEN')
-
-Bot.run(str(token))
