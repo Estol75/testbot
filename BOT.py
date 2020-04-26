73c2172d15b778902e6f74774ba9becfd2314a15
Estol75 <64350850+Estol75@users.noreply.github.com>
2020-04-26 13:02:11 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
-
+import os
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(pass_context=True) #разрешаем передавать агрументы
 async def test(ctx, arg): #создаем асинхронную фунцию бота
     await ctx.send(arg) #отправляем обратно аргумент
+  
+token = os.environ.get('BOT_TOKEN')
 
+bot.run(token)
